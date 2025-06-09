3ebfa10165e38c34cb2900bd85dcda3fc1007f14
tenda-yuki-nozaki <nozaki_yuki@tenda.co.jp>
2025-06-09 12:01:16 +0900

    test tailwind

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,23 @@
-<x-guest-layout>
-    <!-- Session Status -->
+
+
+
+@extends('layouts.app')
+
+@section('title', 'login')
+
+@section('content')
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
+    <div class="m-auto pt-6 pb-6">
+        <h2 class="mx-8">login</h2>
+    </div>
+
+    <div class="bg-white pb-6 pt-6 mr-8 ml-8 pr-6 pl-6">
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <!-- Email Address -->
-        <div>
+        <div  >
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
@@ -38,10 +49,18 @@
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
         </div>
+        <button class="btn-primary mx-auto">
+            {{ __('Log in') }}
+        </button>
     </form>
-</x-guest-layout>
+    <button class="btn-primary mx-auto" onclick="location.href='{{ route('register') }}'">
+        新規会員登録
+    </button>
+
+    </div>
+    <div class="mt-6">
+
+    </div>
+
+@endsection
