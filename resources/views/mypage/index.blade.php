2bbc8689f1c900e4ba66699a801c493ece661606
tenda-luna-okano <okano_runa@tenda.co.jp>
2025-06-09 14:48:05 +0900

    ビューの作成

---
+++ resources/views/mypage/index.blade.php
@@ -0,0 +1,23 @@
+{{--footerを下に固定する必要あり--}}
+{{--マイページの内容を表示するためのビュー--}}
+@extends('layouts.app')
+
+@section('title', 'マイページ')
+
+@section('content')
+    <h1 class="text-center font-bold m-6 text-2xl">マイページ</h1>
+    <div class="border border-[#201a19] rounded-lg m-4 p-4">
+        <div>ここにお知らせなどが来ます。次回のお届け日などなど</div>
+    </div>
+    <div class="border border-[#201a19] ml-4 mr-4">
+        <div class="border-b border-[#201a19] text-center mt-4 pb-4"><span class="material-icons">account_circle</span>マイページトップ</div>
+        <div class="border-b border-[#201a19] text-center mt-4 pb-4"><span class="material-icons">edit</span>登録内容の確認・変更</div>
+        <div class="border-b border-[#201a19] text-center mt-4 pb-4"><span class="material-icons">favorite</span>お気に入り商品</div>
+        <div class="border-b border-[#201a19] text-center mt-4 pb-4"><span class="material-icons">receipt</span>ご注文履歴</div>
+        <div class="text-center mt-4 pb-4"><span class="material-icons">logout</span>退会</div></div>
+    <div class="flex items-center justify-center m-4">
+        <button class="btn-primary">
+            ログアウト
+        </button>
+    </div>
+@endsection
