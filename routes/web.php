dda9c8d48d7d8a06cc6a240bd6a2a052fa97ca00
tenda-luna-okano <okano_runa@tenda.co.jp>
2025-06-09 21:13:19 +0900

    reviews/indexへのルート追加

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/products/index', [ProductsController::class, 'index']);
 require __DIR__.'/auth.php';
+
+Route::get('/reviews/index', function () {
+    return view('reviews.index');
+});
