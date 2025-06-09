b33a53c539281e20c704904a8611aa16b4fcb703
tenda-luna-okano <okano_runa@tenda.co.jp>
2025-06-09 16:38:32 +0900

    クーポン管理画面の作成

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/products/index', [ProductsController::class, 'index']);
 require __DIR__.'/auth.php';
+
+Route::get('/admin/coupons/index',function() {
+    return view('admin.coupons.index');
+});
