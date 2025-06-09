5acdd7679e4ddaeb7f693f36aac0db67420423a9
tenda-luna-okano <okano_runa@tenda.co.jp>
2025-06-09 13:48:31 +0900

    定期便紹介の追加

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('subscription/index', function () {
+    return view('subscription.index');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
