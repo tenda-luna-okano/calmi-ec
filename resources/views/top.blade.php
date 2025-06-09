2e9e703e6613d7917dd34ef665072ed8174462c4
tenda-hiroto-kii <kii_hiroto@tenda.co.jp>
2025-06-09 14:57:27 +0900

    TOPページのviewファイル

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -20,55 +20,55 @@
     画像はpublic/img内に保存して実行(imgフォルダは作成する)
 -->
 
-<center>
-    <div style="height:30px;"></div>
+    <center>
+        <div style="height:30px;"></div>
 
-    <div style="width:90%; border:2px solid #000;">
-        <ul class="slick">
-            <li><img id="スライドショー用１" src="{{ asset('img/スライドショー用１.png') }}" alt=""></li>
-            <li><img id="スライドショー用２" src="{{ asset('img/スライドショー用２.png') }}" alt=""></li>
-        </ul>
-    </div>
+        <div style="width:90%; border:2px solid #201a1e;">
+            <ul class="slick">
+                <li><img id="スライドショー用１" src="{{ asset('img/スライドショー用１.png') }}" alt=""></li>
+                <li><img id="スライドショー用２" src="{{ asset('img/スライドショー用２.png') }}" alt=""></li>
+            </ul>
+        </div>
 
-    <div style="height:25px;"></div>
+        <div style="height:25px;"></div>
 
-    <hr style="width:90%; border:solid #000; height:1px;">
-    <div style="height:25px;"></div>
+        <hr style="width:90%; border:solid #201a1e; height:1px;">
+        <div style="height:25px;"></div>
 
-    <!--
-        LightNight,DeepNight,PremiumNightの文字が横幅少し広いので松竹梅のような短い名前で表したほうがいいかもしれない
-    -->
-    <b style="font-size:30px;">定期便のご紹介</b><br><br>
-    <div class="field">
-        <li style="list-style:none;">LightNight<img id="LightNight" src="{{ asset('img/LightNight.png') }}" alt="" style="width:80%;"></li>
-        <li style="list-style:none;">DeepNight<img id="DeepNight" src="{{ asset('img/DeepNight.png') }}" alt="" style="width:80%;"></li>
-        <li style="list-style:none;">PremiumNight<img id="PremiumNight" src="{{ asset('img/PremiumNight.png') }}" alt="" style="width:80%;"></li>
-    </div>
+        <!--
+            LightNight,DeepNight,PremiumNightの文字が横幅少し広いので松竹梅のような短い名前で表したほうがいいかもしれない
+        -->
+        <b style="font-size:30px;">定期便のご紹介</b><br><br>
+        <div class="field">
+            <li style="list-style:none;">LightNight<img id="LightNight" src="{{ asset('img/LightNight.png') }}" alt="" style="width:80%;"></li>
+            <li style="list-style:none;">DeepNight<img id="DeepNight" src="{{ asset('img/DeepNight.png') }}" alt="" style="width:80%;"></li>
+            <li style="list-style:none;">PremiumNight<img id="PremiumNight" src="{{ asset('img/PremiumNight.png') }}" alt="" style="width:80%;"></li>
+        </div>
+            
+
         
+        <div style="height:25px;"></div>
+        <hr style="width:90%; border:solid #201a1e; height:1px;">
+        <div style="height:25px;"></div>
+        
+        <!--
+            ジャンルごとの写真がまだないのでジャンルの中から一つの写真をピックアップして載せてます
+        -->
 
-    
-    <div style="height:25px;"></div>
-    <hr style="width:90%; border:solid #000; height:1px;">
-    <div style="height:25px;"></div>
-    
-    <!--
-        ジャンルごとの写真がまだないのでジャンルの中から一つの写真をピックアップして載せてます
-    -->
+        <b style="font-size:30px;">ジャンルごとのご紹介</b><br><br>
+        <div class="field">
+            <li style="list-style:none;">アロマ<img id="アロマ" src="{{ asset('img/ピローミスト.png') }}" alt="" style="width:80%;"></li>
+            <li style="list-style:none;">フード<img id="フード" src="{{ asset('img/GABAチョコレート.png') }}" alt="" style="width:80%;"></li>
+        </div>
+        <br>
+        <div class="field">
+            <li style="list-style:none;">タッチ<img id="タッチ" src="{{ asset('img/温感ジェル.png') }}" alt="" style="width:80%;"></li>
+            <li style="list-style:none;">ご褒美スイーツ<img id="ご褒美スイーツ" src="{{ asset('img/マカロン.png') }}" alt="" style="width:80%;"></li>
+        </div>
 
-    <b style="font-size:30px;">ジャンルごとのご紹介</b><br><br>
-    <div class="field">
-        <li style="list-style:none;">アロマ<img id="アロマ" src="{{ asset('img/ピローミスト.png') }}" alt="" style="width:80%;"></li>
-        <li style="list-style:none;">フード<img id="フード" src="{{ asset('img/GABAチョコレート.png') }}" alt="" style="width:80%;"></li>
-    </div>
-    <br>
-    <div class="field">
-        <li style="list-style:none;">タッチ<img id="タッチ" src="{{ asset('img/温感ジェル.png') }}" alt="" style="width:80%;"></li>
-        <li style="list-style:none;">ご褒美スイーツ<img id="ご褒美スイーツ" src="{{ asset('img/マカロン.png') }}" alt="" style="width:80%;"></li>
-    </div>
+        <div style="height:50px;"></div>
 
-    <div style="height:50px;"></div>
-
-</center>
+    </center>
 
 @endsection
 
