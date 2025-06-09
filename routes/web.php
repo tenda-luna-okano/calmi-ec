a9c381f268a8453e6a985c837064d8a5f0e851ce
tenda-luna-okano <okano_runa@tenda.co.jp>
2025-06-09 16:09:50 +0900

    admin/products/insertへのルート追加

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/products/index', [ProductsController::class, 'index']);
 require __DIR__.'/auth.php';
+
+Route::get('/admin/products/insert',function(){
+    return view('admin.products.insert');
+});
