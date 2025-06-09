01c0173660bb675bfcd9cd92c60d7a7261b3bb30
tenda-yuki-nozaki <nozaki_yuki@tenda.co.jp>
2025-06-09 13:47:11 +0900

    これ以上は無理だぁー

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,8 +8,8 @@
 @section('content')
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <div class="pt-6 pb-6 w-full mx-auto">
-        <h2 class=" w-full mx-auto">login</h2>
+    <div class="pt-6 pb-6 flex justify-center">
+        <h2 class=" underline decoration-#201a1e decoration-3 underline-offset-8">　　　login　　　</h2>
     </div>
 
     <div class="bg-white pb-6 pt-6  pr-6 pl-6 w-5/6 mx-auto">
@@ -17,14 +17,14 @@
         @csrf
 
         <!-- Email Address -->
-        <div  >
+        <div class="w-5/6 mx-auto" >
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
+        <div class="mt-4 w-5/6 mx-auto">
             <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
@@ -36,28 +36,28 @@
         </div>
 
         <!-- Remember Me -->
-        <div class="block mt-4">
+        <div class="block mt-4 w-5/6 mx-auto">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-end mt-4 w-5/6 mx-auto">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
         </div>
-        <button class="btn-primary mx-auto w-100 mb-4">
+        <button class="btn-primary mx-auto  mb-4 mt-4 flex justify-center w-140">
             {{ __('Log in') }}
         </button>
     </form>
-    <div class="divide-y divide-white-400 w-5/6 mx-auto">
+    <div class="divide-y divide-white-400 flex justify-center">
         <div ></div>
-        <div class="w-5/6 mx-auto">
-            <button class="btn-primary mt-4 w-100 mx-auto" onclick="location.href='{{ route('register') }}'">
+        <div class="">
+            <button class="btn-primary mt-4 w-140" onclick="location.href='{{ route('register') }}'">
                 新規会員登録
             </button>
         </div>
