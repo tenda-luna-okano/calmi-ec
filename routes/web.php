a4837502cc5d13346fa118bdfc237f2ae500aacc
tenda-luna-okano <okano_runa@tenda.co.jp>
2025-06-10 10:12:17 +0900

    ルートの追加

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/products/index', [ProductsController::class, 'index']);
 require __DIR__.'/auth.php';
+Route::get('/admin/sales/index', function() {
+    return view('admin.sales.index');
+});
