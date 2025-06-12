28084fa7bf68437fb89c257d23a43ef7b742ffb9
tenda-hiroto-kii <kii_hiroto@tenda.co.jp>
2025-06-12 15:07:37 +0900

    First Commit

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -10,7 +10,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 /**
  * Class Customer
  *
--- database/migrations/2025_06_10_180303_create_payments_table.php
+++ database/migrations/2025_06_10_180303_create_payments_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('payment_name')->comment('支払方法名');
             $table->bigInteger('card_number')->nullable()->comment('カード番号');
             $table->integer('expire')->nullable()->comment('有効期限');
-            $table->string('card_customer_name')->nullable()->comment('カード名義');
+            $table->string('card_customer_name')->nullable()->comment('カード名義')->nullable();
             $table->boolean('can_use_flg')->comment('有効な支払方法フラグ');
             $table->timestamps();
         });
