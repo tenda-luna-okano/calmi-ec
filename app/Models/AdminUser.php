a89416c87fd281136dc33ad7d5dc4b9b54f2f553
tenda-yuki-nozaki <nozaki_yuki@tenda.co.jp>
2025-06-12 22:43:23 +0900

    done make admin login

--- app/Models/AdminUser.php
+++ app/Models/AdminUser.php
@@ -7,36 +7,38 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 /**
  * Class AdminUser
- * 
+ *
  * @property int $admin_user_id
  * @property string $admin_email
  * @property string $admin_password
  * @property string $admin_name
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
- * 
+ *
  * @property Collection|Column[] $columns
  * @property Collection|Notification[] $notifications
  *
  * @package App\Models
  */
-class AdminUser extends Model
+class AdminUser extends Authenticatable
 {
 	protected $table = 'admin_users';
 	protected $primaryKey = 'admin_user_id';
 
 	protected $hidden = [
-		'admin_password'
+		'password'
 	];
 
 	protected $fillable = [
-		'admin_email',
-		'admin_password',
+		'email',
+		'password',
 		'admin_name'
 	];
 
