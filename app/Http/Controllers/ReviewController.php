28745ee0e55b05e55f3933bf201f6762fde28766
tenda-luna-okano <okano_runa@tenda.co.jp>
2025-06-12 16:01:38 +0900

    item_idの追加

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -11,13 +11,14 @@ class ReviewController extends Controller
     {
        $validated = $request->validate([
         'review_name' => 'required|string|max:100',
-        'review_item_id' => 'required|int',
         'customer_nickname' => 'required|string|max:50',
         'customer_mail' => ['required', 'string', 'email:strict,dns','max:255'],
         'review_star' => 'required|integer|min:1|max:5',
         'review_content' => 'required|string|max:1000',
         'reviewer_age' => 'required|in:10,20,30,40,50,60',
     ]);
+     $validated['review_item_id'] = $request->item_id;
+
     Review::create($validated);
 
     return redirect()->back()->with('message', 'レビューを投稿しました！');  
