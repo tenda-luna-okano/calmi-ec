d461796141ed879c6e4b7e9a4a80b7dfdfc0cde4
tenda-hiroto-kii <kii_hiroto@tenda.co.jp>
2025-06-17 13:33:08 +0900

    定期便のビュー追加

--- resources/views/subscription/edit.blade.php
+++ resources/views/subscription/edit.blade.php
@@ -39,16 +39,23 @@
     <!--定期便変更用のポップアップ-->
     <div class="change popup">
         <p>定期便を変更しますか</p>
-        <button class="yes">はい</button>
+        <br>
+        <form action="{{route('subscription.update')}}" method="POST">
+            @csrf
+            <input type="hidden" name="subscriptionID" id="hiddenSubscriptionID" value="">
+            <button type="submit" class="yes">はい</button>
+        </form>
         <button class="no">いいえ</button>
-        <button class="cancel">キャンセル</button>
+        <br><button class="cancel">キャンセル</button>
     </div>
     <!--定期便削除用のポップアップ-->
     <div class="delete popup">
         <p>定期便を削除しますか</p>
-        <button class="yes">はい</button>
-        <button class="no">いいえ</button>
-        <button class="cancel">キャンセル</button>
+        <form action="{{route('subscription.destroy')}}">
+            <button class="yes">はい</button>
+        </form>
+        <br><button class="no">いいえ</button>
+        <br><button class="cancel">キャンセル</button>
     </div>
 
     <!--登録している定期便を表示させる必要あり-->
@@ -60,18 +67,18 @@
     <div class="field">
         <div style="width:40%;">
             <div style="height:60px; border:2px solid #201a1e; background-color:white;">
-                定期便名<br>～
+                定期便名<br>{{$subscription->subscribeDetailMaster->subscribe_item_name}}
             </div><br>
             <div style="height:60px; border:2px solid #201a1e; background-color:white;">
-                お支払方法<br>～
+                お支払方法<br>{{$payment}}
             </div><br>
             <div style="height:60px; border:2px solid #201a1e; background-color:white;">
-                次回の更新日<br>～
+                次回の更新日<br>{{$subscription->subscribe_end_day}}
             </div><br>
         </div>
 
         <div style="width:50%; height:200px">
-            <img id="LightNight" src="{{ asset('img/LightNight.png') }}" alt="" style="height:90%;">
+            <img id="LightNight" src="{{ asset($subscription->subscribeDetailMaster->subscribe_img) }}" alt="" style="height:90%;">
         </div>
     </div>
     </center>
@@ -85,14 +92,12 @@
     <br><br>
     <center>
     <div class="field">
-        <div>
-            <img id="DeepNight" src="{{ asset('img/DeepNight.png') }}" alt="" style="width:80%;"><br>
-            <button class="change_subscription" style="background-color:#d0b49f; height:40px; width:60%; color:white;">変更する</button>
-        </div>
-        <div>
-            <img id="PremiunNight" src="{{ asset('img/PremiumNight.png') }}" alt="" style="width:80%;"><br>
-            <button class="change_subscription" style="background-color:#d0b49f; height:40px; width:60%; color:white;">変更する</button>
-        </div>
+        @foreach($subscriptionImg as $img)
+            <div>
+                <img id="DeepNight" src="{{ asset($img['img']) }}" alt="" style="width:80%;"><br>
+                <button class="change_subscription" style="background-color:#d0b49f; height:40px; width:60%; color:white;" name="subscriptionID", data-product-id="{{$img['id']}}">変更する</button>
+            </div>
+        @endforeach
     </div>
     </center>
     <br>
