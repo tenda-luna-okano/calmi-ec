ba8068aed04aaf706dff134932199de478da2114
tenda-luna-okano <okano_runa@tenda.co.jp>
2025-06-18 14:41:14 +0900

    ビューの修正

--- resources/views/subscription/edit.blade.php
+++ resources/views/subscription/edit.blade.php
@@ -50,7 +50,7 @@
     </div>
     <!--定期便削除用のポップアップ-->
     <div class="delete popup">
-        <p>定期便を削除しますか</p>
+        <p>定期便を解約しますか</p>
         <form action="{{route('subscription.destroy')}}">
             <button class="yes">はい</button>
         </form>
@@ -61,18 +61,18 @@
     <!--登録している定期便を表示させる必要あり-->
     <br>
     　
-    <span style="font-size:20px; text-decoration:underline; text-underline-offset: 5px;">ご登録中の定期便内容</span>
+    <span style="font-size:20px; text-decoration:underline; text-underline-offset: 5px;" class="flex justify-center">ご登録中の定期便内容</span>
     <br><br>
     <center>
     <div class="field">
-        <div style="width:40%;">
-            <div style="height:60px; border:2px solid #201a1e; background-color:white;">
+        <div style="width:40%;" class="ml-4">
+            <div class="shadow-sm rounded" style="height:60px; border:1px solid #201a1e; background-color:white;">
                 定期便名<br>{{$subscription->subscribe_detail_master->subscribe_item_name}}
             </div><br>
-            <div style="height:60px; border:2px solid #201a1e; background-color:white;">
+            <div class="shadow-sm rounded" style="height:60px; border:1px solid #201a1e; background-color:white;">
                 お支払方法<br>{{$payment}}
             </div><br>
-            <div style="height:60px; border:2px solid #201a1e; background-color:white;">
+            <div class="shadow-sm rounded" style="height:60px; border:1px solid #201a1e; background-color:white;">
                 次回の更新日<br>{{$subscription->subscribe_end_day}}
             </div><br>
         </div>
@@ -85,10 +85,10 @@
 
 
     <!--登録している定期便の内容によって変更する定期便を変える必要あり-->
-    <center><hr style="width:90%; border:solid #201a1e; height:1px;"></center>
+    <div class="border-[#D8CFCF]">
     <br>
-    　
-    <span style="font-size:20px; text-decoration:underline; text-underline-offset: 5px;">定期便の変更</noframes></span>
+
+    <span style="font-size:20px; text-decoration:underline; text-underline-offset: 5px;" class="flex justify-center">定期便の変更</noframes></span>
     <br><br>
     <center>
     <div class="field">
@@ -96,18 +96,17 @@
             <div>
                 <img id="DeepNight" src="{{ asset($img['img']) }}" alt="" style="width:80%;"><br>
                 <p>{{$img['name']}}</p>
-                <button class="change_subscription" style="background-color:#d0b49f; height:40px; width:60%; color:white;" name="subscriptionID", data-product-id="{{$img['id']}}">変更する</button>
+                <button class="change_subscription rounded m-2" style="background-color:#d0b49f; height:40px; width:60%; color:white;" name="subscriptionID", data-product-id="{{$img['id']}}">変更する</button>
             </div>
         @endforeach
     </div>
     </center>
     <br>
-    <center><hr style="width:90%; border:solid #201a1e; height:1px;"></center>
+
     <br>
-    　
-    <span style="font-size:20px; text-decoration:underline; text-underline-offset: 5px;">定期便の解除</noframes></span>
+    <span style="font-size:20px; text-decoration:underline; text-underline-offset: 5px;" class="flex justify-center">定期便の解除</noframes></span>
     <br><br><br>
-    <center><button class="delete_subscription" style="background-color:#d0b49f; height:70px; width:80%; color:white;">登録中の定期便を解除する</button></center>
+    <center><button class="delete_subscription btn-primary" style="height:70px; width:80%; color:white;" ">登録中の定期便を解除する</button></center>
     <br><br>
 
 </body>
